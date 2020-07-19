82c5967fdb7dd8511343b1fd82c26ad2b6531b42
Stefan Buhrmester <buhrmi@gmail.com>
2020-07-19 21:19:13 +0100

    indentation

--- src/Coverflow.svelte
+++ src/Coverflow.svelte
@@ -1,8 +1,3 @@
-<!--
-CoverFlow implementation using slot props and experimenting with different types of crossfades.
-https://svelte.dev/tutorial/slot-props
--->
-
 <script>
 import Swipeable from './Swipeable.svelte'
 import { onMount } from 'svelte'
@@ -34,70 +29,69 @@ let showdetails = true
 // i is the cover index
 // progress is a value from (0..covers.length)
 function style(i, progress) {
-	let offset = (i - progress)
-	let blend = offset / 2 +0.5
-	if (blend < 0) blend = 0
-	if (blend > 1) blend = 1
-	let rotateY = rotation - 2 * blend * rotation
-	let transformOrigin = blend * 100
-	return `left: ${offset * gap}vw; transform: rotateY(${rotateY}deg); transform-origin: ${transformOrigin}%`;
+  let offset = (i - progress)
+  let blend = offset / 2 +0.5
+  if (blend < 0) blend = 0
+  if (blend > 1) blend = 1
+  let rotateY = rotation - 2 * blend * rotation
+  let transformOrigin = blend * 100
+  return `left: ${offset * gap}vw; transform: rotateY(${rotateY}deg); transform-origin: ${transformOrigin}%`;
 }
 </script>
 
 <div class="background">
 
-	<h2>
-	  {covers[current].name}
-	</h2>
-	
-	<div class="coverflow">
-   <Swipeable
-			speed="4"
-			numStates={covers.length}
-			bind:current
-			let:jump
-			let:progress>
+  <h2>
+    {covers[current].name}
+  </h2>
 
-			{#each covers as cover, i}
-				<div class="cover" on:click={() => jump(i)} style={style(i, progress)}>
-					<div class="img" style="background-image: url({cover.url})"/>
-					<div class="img reflection" style="background-image:url({cover.url})"/>
-				</div> 
-			{/each}	
-		</Swipeable>	
-	</div>
+  <div class="coverflow">
+    <Swipeable
+      speed="4"
+      numStates={covers.length}
+      bind:current
+      let:jump
+      let:progress>
+ 
+  	  {#each covers as cover, i}
+        <div class="cover" on:click={() => jump(i)} style={style(i, progress)}>
+          <div class="img" style="background-image: url({cover.url})"/>
+          <div class="img reflection" style="background-image:url({cover.url})"/>
+        </div> 
+      {/each}	
+    </Swipeable>	
+  </div>
 
-	<div class="gradient" />
-
+  <div class="gradient" />
 </div>
 
 
 <style>
 .background {
-	background: #555562;
-	position: absolute;
-	top: 0;
-	left: 0;
-	bottom: 0;
-	right: 0;
-	text-align: center;
-	color: white;
-	overflow: hidden;
+  background: #555562;
+  position: absolute;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  text-align: center;
+  color: white;
+  overflow: hidden;
   perspective: 800px;
 }
 .coverflow {
   margin: 0 auto;
-	margin-top: 20px;
+  margin-top: 20px;
   display: inline-block;
   position: absolute;
-	left: 50%;
+  left: 50%;
   height: 48vw;
-	max-height: 400px;
+  max-height: 400px;
   width: 70vw;
-	max-width: 600px;
+  max-width: 600px;
   transform-style: preserve-3d;
-	transform: translateX(-50%) translateZ(-100px);
-	user-select: none;
+  transform: translateX(-50%) translateZ(-100px);
+  user-select: none;
 }
 .cover {
   height: 100%;
@@ -118,16 +112,16 @@ function style(i, progress) {
 }
 
 .gradient {
-	width: 100%;
-	height: 100%;
-	top: 50%;
-	left: 0;
-	position: absolute;
-	background: linear-gradient(rgba(85,85,98,0) 0%, rgba(85,85,98,0.5) 20%,rgba(85,85,98,1) 50%);
-	pointer-events: none;
+  width: 100%;
+  height: 100%;
+  top: 50%;
+  left: 0;
+  position: absolute;
+  background: linear-gradient(rgba(85,85,98,0) 0%, rgba(85,85,98,0.5) 20%,rgba(85,85,98,1) 50%);
+  pointer-events: none;
 }
-	h2 {
-		margin-top: 40px;
-		margin-bottom: 0px;
-	}
+h2 {
+  margin-top: 40px;
+  margin-bottom: 0px;
+}
 </style>
